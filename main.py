c4577fe94d9a3c99d8d03a312707ca4ee76e1378
pqponet <pqponet@gmail.com>
2025-02-22 17:26:24 +0800

    Access OpenAi

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
+import json
+import os
 import streamlit as st
 import componts
 from datetime import datetime
 import service
 
-default_prompt = "下面是最近发生的新闻，帮我总结提取一下新闻内容，通过你的阅读需要你给出判断是否会对某只或者某类股票造成影响，以及对黄金价格走势的影响。影响需要给出新闻依据。"
+default_prompt = f"""
+你是一位资深财经分析师，擅长从海量信息中快速识别投资机会与潜在风险, 并给出相应的投资建议。请基于我提供的新闻数据，执行以下分析：
+1. 关键事件筛选
+识别影响宏观经济/行业/资本市场的政策类新闻（如货币政策、行业规范）
+标记涉及贵金属供需的核心事件（如矿山罢工、新能源需求）
+捕捉地缘政治/国际经贸重大变动（如中美关税、战争风险）
+2. 影响链分析
+政策类：使用「政策工具→流动性变化→行业传导」框架
+（例：降准→银行放贷能力+15%→基建/房地产板块受益）
+贵金属类：构建「供给端事件+需求端动向+美元指数」三维评估
+国际事件：标注受影响的具体商品/汇率/跨国企业
+3. 投资建议输出要求
+每条建议必须包含：
+机会/风险类型（政策套利/事件驱动/周期反转等）
+具体标的（如黄金期货、某行业ETF、个股）
+逻辑链条（使用"因A导致B进而影响C"结构）
+关联新闻的标题
+----------
+示例输出
+[机会] 贵金属短期做多机会
+标的：COMEX黄金期货
+逻辑链：①南非主要金矿罢工（矿业周刊）→黄金供应量预期减少4% ②美联储加息放缓信号（路透社）→美元指数走弱压力增大
+操作建议：建议在1980美元/盎司支撑位建立3个月期货多头
+[风险] 新能源车板块回调风险
+标的：中证新能源汽车指数
+逻辑链：①锂电池原材料碳酸锂库存创两年新高（财联社）→成本端支撑减弱 ②欧盟反补贴调查升级（BBC）→出口份额可能收缩
+风险提示：建议减持相关ETF并设置8%止损线
+
+请特别注意新闻中涉及[特定关注领域，如锂电池/农业供给侧]的条目，并做深度关联分析。
+请用结构化排版回复，避免专业术语堆砌，保持信息密度与可读性平衡。
+"""
 
 processResult = None
 
+auth_key = os.environ.get("FA_AUTH_KEY")
+if not auth_key:
+    auth_key = '123'
+
+auth_check_pass = False
+if 'auth_check_pass' in st.session_state:
+    auth_check_pass = st.session_state.auth_check_pass
+if auth_key and not auth_check_pass:
+    with st.form("auth_check"):
+        check_auth_key = st.text_input("请输入访问密码", type="password")
+        submitted = st.form_submit_button("提交")
+        if submitted:
+            if check_auth_key != auth_key:
+                st.error("密码校验失败！")
+                st.stop()
+            else:
+                st.session_state.auth_check_pass = True
+        else:
+            st.stop()
+
 with st.sidebar:
-    api_key = st.text_input("Api Key", type="password")
-    base_url = st.text_input("Base Url", "https://api.deepseek.com")
-    mode_name = st.text_input("Model Name", "deepseek-r1")
+    api_key = st.text_input("Api Key", os.environ.get("OPENAI_API_KEY"), type="password")
+    base_url = st.text_input("Base Url", os.environ.get("OPENAI_BASE_URL"))
+    mode_name = st.text_input("Model Name", os.environ.get("OPENAI_MODEL_NAME"))
     prompt = st.text_area("Prompt", default_prompt, height=300)
 
 with st.expander("沪深300&市场情绪指数", expanded=False, icon="🔥"):
@@ -60,7 +112,25 @@ def check_llm_input():
         st.stop()
 
 
+def get_news_input_list():
+    result = []
+    for n_item in news_list:
+        for index, row in n_item.iterrows():
+            result.append(json.loads(row.drop(labels=['url']).to_json(force_ascii=False)))
+    return result
+
+
 submitted = st.button("🚀 新闻分析", use_container_width=True)
 if submitted:
     check_llm_input()
-    st.info(prompt)
+    input_list = get_news_input_list()
+    if len(input_list) <= 0:
+        st.error("暂未查询到新闻数据!!!")
+        st.stop()
+    input_text = json.dumps(input_list, ensure_ascii=False)
+    response = service.generate_response(input_text, prompt, api_key, base_url, mode_name)
+    with st.status("正在分析...") as status:
+        st.write_stream(response)
+        status.update(
+            label="分析完成", state="complete", expanded=True
+        )
--- service.py
+++ service.py
@@ -1,5 +1,20 @@
 from pathlib import Path
 import pandas as pd
+from openai import OpenAI
+
+system_prompt = """
+我会提供一些我关注的新闻数据，以JsonArray的格式给你，示例如下：
+[
+    {
+        "source": "{示例新闻来源}",
+        "category": "{示例类别}",
+        "pub_time": "{示例发布日期}",
+        "title": "{示例标题}",
+        "content": "{示例内容}"
+    }
+]
+基于这些新闻数据，需要你通过仔细阅读分析，为我完成以下任务。
+"""
 
 
 def read_csv(file_str: Path):
@@ -28,3 +43,46 @@ def load_news(date_str: str, category: str):
         if item is not None:
             result.append(item)
     return result
+
+
+def analysis_chunk(chunk, key: str):
+    try:
+        if key == 'reasoning_content':
+            return chunk.choices[0].delta.reasoning_content
+        elif key == 'content':
+            return chunk.choices[0].delta.content
+    except Exception as e:
+        print(f"analysis_chunk failed for {key}, {e}")
+
+
+def generate_response(input_text: str,
+                      prompt: str,
+                      api_key: str,
+                      base_url: str,
+                      model_name: str):
+    client = OpenAI(api_key=api_key, base_url=base_url)
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": f"{system_prompt}, 我的任务是：{prompt}"},
+            {"role": "assistant", "content": "好呀，请你提供一些相关的新闻，这样我就能按照要求进行分析啦。"},
+            {"role": "user", "content": f"新闻数据如下，开始完成你的任务：{input_text}"},
+        ],
+        temperature=0.6,
+        stream=True
+    )
+    thinking = False
+    try:
+        for chunk in response:
+            if reasoning_content := analysis_chunk(chunk, 'reasoning_content'):
+                if not thinking:
+                    thinking = True
+                    yield "<思考中>"
+                yield reasoning_content
+            elif content := analysis_chunk(chunk, 'content'):
+                if thinking:
+                    thinking = False
+                    yield "</思考结束>"
+                yield content
+    except Exception as e:
+        print(f"出现错误：{e}")
